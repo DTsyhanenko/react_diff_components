3d62285531c7c6082754b1fa9cbafb6aa2d3472d
DMYTRO TSYHANENKO <DMYTRO.TSYHANENKO@OUTLOOK.COM>
2023-06-09 10:39:16 +0200

    navi done

--- src/pages/ButtonPage.js
+++ src/pages/ButtonPage.js
@@ -2,32 +2,33 @@ import { GoBell, GoCloudDownload, GoBriefcase } from "react-icons/go";
 import Button from "../components/Button";
 
 function ButtonPage() {
+    const handleClick = () => {};
 
     return (
         <div>
             <div>
-                <Button secondary outline rounded className="mb-5">
+                <Button secondary outline rounded onClick={handleClick}>
                     <GoBell />
                     Click 1
                 </Button>
             </div>
             <div>
-                <Button danger outline>
+                <Button danger outline onClick={handleClick}>
                     <GoCloudDownload />
                     Click 2
                 </Button>
             </div>
             <div>
-                <Button warning>
+                <Button warning onClick={handleClick}>
                     <GoBriefcase />
                     Click 3
                 </Button>
             </div>
             <div>
-                <Button success outline>Click 4</Button>
+                <Button success outline onClick={handleClick}>Click 4</Button>
             </div>
             <div>
-                <Button primary rounded>Click 5</Button>
+                <Button primary rounded onClick={handleClick}>Click 5</Button>
             </div>
         </div>
     );
